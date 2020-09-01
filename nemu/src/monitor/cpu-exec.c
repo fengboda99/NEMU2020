8aecb2ddcd22f14bcdb9764de710804cdc62fe57
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-01 07:15:30 +0000

    >  compile NEMU 3018001662 simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  07:15:30 up  5:35,  1 user,  load average: 0.08, 0.20, 0.20 9c4301efa7eb30c5112cdf449b7df0f18fbad916

--- nemu/src/monitor/cpu-exec.c
+++ nemu/src/monitor/cpu-exec.c
@@ -70,6 +70,7 @@ void cpu_exec(volatile uint32_t n) {
 		if(n_temp < MAX_INSTR_TO_PRINT) {
 			printf("%s\n", asm_buf);
 		}
+		else printf("yjjnb\n");
 #endif
 
 		/* TODO: check watchpoints here. */
