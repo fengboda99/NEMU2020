47eb08e3b0d779d148a910f7a6864dd30525ad18
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-02 11:58:54 +0000

    >  compile NEMU 3018001662 simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  11:58:54 up 1 day, 10:18,  1 user,  load average: 0.42, 0.29, 0.20 8540d359da127ead53373041a1d49b571cb49fd

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -7,7 +7,7 @@
 #include <regex.h>
 
 enum {
-	NOTYPE = 256, EQ, NUMBER_1, MINUS
+	NOTYPE = 256, EQ, NUMBER_1, MINUS, NUMBER_2, POINTER, REGISTER, NEQ, AND, OR
 
 	/* TODO: Add more token types */
 
@@ -16,23 +16,28 @@ enum {
 static struct rule {
 	char *regex;
 	int token_type;
+	int prior;
 } rules[] = {
 
 	/* TODO: Add more rules.
 	 * Pay attention to the precedence level of different rules.
 	 */
 
-	{" +",	NOTYPE},				// spaces
-	{"\\+", '+'},					// plus
-	{"-", '-'}, 					// sub
-	{"\\*", '*'},					// mul
-	{"/", '/'},					// div
-	{"\\(", '('},					//left
-	{"\\)",')'},					//right
-	{"\\b[0-9]+\\b", NUMBER_1},			//number
-	//{"\\b0[xX][0-9a-zA-Z]+\\b", NUMBER_2},		//16number
-	//{"\\b[]", var},
-	{"==", EQ}					// equal
+	{" +",	NOTYPE, 0},					// spaces
+	{"\\+", '+', 3},					// plus
+	{"-", '-', 3}, 						// sub
+	{"\\*", '*', 4},					// mul
+	{"/", '/', 4},						// div
+	{"\\(", '(', 6},					//left
+	{"\\)",')', 6},						//right
+	{"\\b[0-9]+\\b", NUMBER_1, 0},				//number
+	{"\\b0[xX][0-9a-zA-Z]+\\b", NUMBER_2, 0},		//16number
+	{"\\$[a-zA-Z]", REGISTER, 0},                      	//register
+	{"!=", NEQ, 2},  					//not equal
+	{"!", '!', 5},						//not
+	{"&&", AND, 1},						//AND
+	{"\\|\\|", OR, 0},					//OR
+	{"==", EQ, 2}						// equal
 };
 
 #define NR_REGEX (sizeof(rules) / sizeof(rules[0]) )
@@ -59,6 +64,7 @@ void init_regex() {
 typedef struct token {
 	int type;
 	char str[32];
+	int prior;
 } Token;
 
 Token tokens[32];
@@ -91,6 +97,7 @@ static bool make_token(char *e) {
 					
 					default: 
 						tokens[nr_token].type = rules[i].token_type;
+						tokens[nr_token].prior = rules[i].prior;
 						strncpy(tokens[nr_token].str,substr_start,substr_len);
 						tokens[nr_token].str[substr_len]='\0';
 						nr_token++;
@@ -124,7 +131,7 @@ bool check_parentheses(int l,int r) {
 }
 
 int dominant_operator(int l,int r) {
-	int position = -1;
+	int position = l;
 	int i;
 	int cnt = 0; // count the parentheses
 	for(i=r;i>=l;i--) {
@@ -132,14 +139,9 @@ int dominant_operator(int l,int r) {
 		if(tokens[i].type == ')') cnt++;
 		else if(tokens[i].type == '(') cnt--;
 		if(cnt==0) {
-			if(position == -1) position = i;
-			else if(tokens[position].type==MINUS) {
-				if(tokens[i].type!='('&&tokens[i].type!=')') 
-					position = i;
-			}
-			else if(tokens[position].type=='*' || tokens[i].type=='/') {
-				if(tokens[i].type == '+' || tokens[i].type == '-')
-					position = i;
+			if(position==l&&i!=l) position = i;
+			else if(tokens[i].prior>tokens[position].prior) {
+				position = i;
 			}
 		}
 	}
@@ -152,16 +154,49 @@ uint32_t eval(int l,int r) {
 		return 0;
 	}
 	else if(l==r) {
-		int value;
-		sscanf(tokens[l].str,"%d",&value);
+		int value=-1;
+		if(tokens[l].type==NUMBER_1)
+			sscanf(tokens[l].str,"%d",&value);
+		else if(tokens[l].type==NUMBER_2)
+			sscanf(tokens[l].str,"%x",&value);
+		else if(tokens[l].type==REGISTER) {
+			char *tmp = tokens[l].str+1;
+			int i;
+			for(i=0;i<8;i++) {
+				if(strcmp(tmp,regsl[i])==0) {
+					value = reg_l(i);
+					break;
+				}
+				if(strcmp(tmp,regsw[i])==0) {
+					value = reg_w(i);
+					break;
+				}	
+				if(strcmp(tmp,regsb[i])==0) {
+					value = reg_b(i);
+					break;
+				}
+			}
+			if(i==8) {
+				if(strcmp(tmp,"eip")==0) {
+					value = cpu.eip;
+				}
+			}
+			else assert(1); 	
+		}
+		else assert(1);
 		return value;
 	}
 	else if(check_parentheses(l,r)==true) return eval(l+1,r-1);
 	else {
 		int position = dominant_operator(l,r);
-		if(tokens[position].type == MINUS) {
+		if(tokens[position].type==MINUS||tokens[position].type==POINTER||tokens[position].type=='!') {
 			int val = eval(position+1,r);
-			return -val;
+			switch(tokens[position].type) {
+				case MINUS: return -val;
+				case POINTER: return swaddr_read(val,4);
+				case '!': return !val;
+				default: break;
+			}
 		}
 		int val1 = eval(l,position-1);
 		int val2 = eval(position+1,r);
@@ -170,6 +205,10 @@ uint32_t eval(int l,int r) {
 			case '-': return val1-val2;
 			case '*': return val1*val2;
 			case '/': return val1/val2;
+			case AND: return val1&&val2;
+			case OR: return val1||val2;
+			case EQ: return val1==val2;
+			case NEQ: return val1!=val2;
 			default: break;
 		}
 	} 
@@ -186,8 +225,14 @@ uint32_t expr(char *e, bool *success) {
 	/* TODO: Insert codes to evaluate the expression. */
 	int i;
 	for(i=0;i<nr_token;i++) {
-		if(tokens[i].type=='-'&&(i==0||(tokens[i-1].type!=NUMBER_1&&tokens[i].type!=')')))
+		if(tokens[i].type=='-'&&(i==0||(tokens[i-1].type!=NUMBER_1&&tokens[i-1].type!=')'&&tokens[i-1].type!=NUMBER_2&&tokens[i-1].type!=REGISTER))) {
 			tokens[i].type = MINUS;
+			tokens[i].type = 6;
+		}
+		if(tokens[i].type=='*'&&(i==0||(tokens[i-1].type!=NUMBER_1&&tokens[i-1].type!=')'&&tokens[i-1].type!=NUMBER_2&&tokens[i-1].type!=REGISTER))) {
+			tokens[i].type = POINTER;
+			tokens[i].prior = 6;
+		}
 	}
 	*success = true;
 	return eval(0,nr_token-1);
