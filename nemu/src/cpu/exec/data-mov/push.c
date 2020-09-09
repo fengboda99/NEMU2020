65a6c8f50643789df09185a3c48d18df61dff048
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-09 03:51:10 +0000

    >  compile NEMU 3018001662 simpleedu Linux simpleedu 4.15.0-115-generic #116-Ubuntu SMP Wed Aug 26 14:04:49 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  03:51:10 up 1 day, 15:58,  1 user,  load average: 0.56, 0.44, 0.27 925f2a0b955b1c6e48d85caffc084f40dc18f6cf

---
+++ nemu/src/cpu/exec/data-mov/push.c
@@ -0,0 +1,11 @@
+#include "cpu/exec/helper.h"
+
+#define DATA_BYTE 2
+#include "push-template.h"
+#undef DATA_BYTE
+
+#define DATA_BYTE 4
+#include "push-template.h"
+#undef DATA_BYTE
+
+make_helper_v(push_r);
