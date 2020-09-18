5a83ee8a1fbe91a4806e067d108b5fe9b42f58ea
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-18 08:44:32 +0000

    >  compile NEMU 3018001662 simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  08:44:32 up 3 days,  8:27,  1 user,  load average: 0.46, 0.41, 0.27 45d02727caa90844942d71184e0d1b9a5d7cd17

--- nemu/src/cpu/exec/logic/test-template.h
+++ nemu/src/cpu/exec/logic/test-template.h
@@ -5,7 +5,7 @@
 static void do_execute() {
 	cpu.CF = 0;
 	cpu.OF = 0;
-	DATA_TYPE ans = op_dest->val & op_src2->val;
+	int ans = op_dest->val & op_src->val;
 	if(ans<0) cpu.SF = 1;
 	else cpu.SF = 0;
 	if(ans==0) cpu.ZF = 1;
