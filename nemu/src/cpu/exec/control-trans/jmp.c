2e36199166938a88a19fb924e8de7a8bd7c5e773
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-18 02:43:18 +0000

    >  compile NEMU 3018001662 simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  02:43:18 up 3 days,  2:26,  1 user,  load average: 1.54, 0.61, 0.38 9eb702a865317e749bd56880c67a172a85bfa7a5

--- nemu/src/cpu/exec/control-trans/jmp.c
+++ nemu/src/cpu/exec/control-trans/jmp.c
@@ -4,4 +4,14 @@
 #include "jmp-template.h"
 #undef DATA_BYTE
 
+#define DATA_BYTE 2
+#include "jmp-template.h"
+#undef DATA_BYTE
+
+#define DATA_BYTE 4
+#include "jmp-template.h"
+#undef DATA_BYTE
+
+make_helper_v(jmp_i);
+
 
--- nemu/src/cpu/exec/control-trans/jmp.h
+++ nemu/src/cpu/exec/control-trans/jmp.h
@@ -2,5 +2,5 @@
 #define __JMP_H__
 
 make_helper(jmp_i_b);
-
+make_helper(jmp_i_v);
 #endif
