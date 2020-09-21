47f78df9a0d6f8125b2dfdb49502fe8bcfe00b68
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-21 14:34:53 +0000

    >  compile NEMU 3018001662 simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  14:34:53 up 6 days, 14:18,  1 user,  load average: 0.68, 0.40, 0.26 3eac08cdebe42a407b90081bc7a1c6c3b80f5003

--- nemu/src/cpu/exec/arith/inc-template.h
+++ nemu/src/cpu/exec/arith/inc-template.h
@@ -7,7 +7,20 @@ static void do_execute () {
 	OPERAND_W(op_src, result);
 
 	/* TODO: Update EFLAGS. */
-	panic("please implement me");
+	int d1 = 1;
+	int d2 = op_src->val;
+	int ans = result;
+	cpu.ZF = !ans;
+	cpu.SF = ans<0?1:0;
+ 	int n =ans;
+	int cnt =0;
+	while(n) {
+		n = n&(n-1);
+		cnt++;
+	}	
+	cpu.PF = cnt%2==0? 1:0;
+	if((d1>0&&d2>0&&ans<=0)||(d2<0&&d1<0&&ans>=0)) cpu.OF = 1;
+	else cpu.OF = 0;
 
 	print_asm_template1();
 }
--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -74,12 +74,12 @@ make_group(group3_v,
 
 /* 0xfe */
 make_group(group4,
-	inv, dec_rm_b, inv, inv, 
+	inc_rm_b, dec_rm_b, inv, inv, 
 	inv, inv, inv, inv)
 
 /* 0xff */
 make_group(group5,
-	inv, dec_rm_v, inv, inv, 
+	inc_rm_v, dec_rm_v, inv, inv, 
 	jmp_rm_v, inv, push_rm_v, inv)
 
 make_group(group6,
@@ -110,8 +110,8 @@ helper_fun opcode_table [256] = {
 /* 0x34 */	inv, inv, inv, inv,
 /* 0x38 */	cmp_r2rm_b, cmp_r2rm_v, cmp_rm2r_b, cmp_rm2r_v,
 /* 0x3c */	cmp_i2a_b, cmp_i2a_v, inv, inv,
-/* 0x40 */	inv, inv, inv, inv,
-/* 0x44 */	inv, inv, inv, inv,
+/* 0x40 */	inc_r_v, inc_r_v, inc_r_v, inc_r_v,
+/* 0x44 */	inc_r_v, inc_r_v, inc_r_v, inc_r_v,
 /* 0x48 */	dec_r_v, dec_r_v, dec_r_v, dec_r_v,
 /* 0x4c */	dec_r_v, dec_r_v, dec_r_v, dec_r_v,
 /* 0x50 */	push_r_v, push_r_v, push_r_v, push_r_v,
