2b9f9de404317c568dee10e857ac54e576de9aad
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-21 06:40:28 +0000

    >  compile NEMU 3018001662 simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  06:40:28 up 6 days,  6:23,  1 user,  load average: 0.27, 0.19, 0.10 700be1a8e8c2eb548d229a13aad121a17e8f027

--- nemu/src/cpu/exec/control-trans/ja-template.h
+++ nemu/src/cpu/exec/control-trans/ja-template.h
@@ -5,7 +5,7 @@
 static void do_execute() {
 	DATA_TYPE_S d = op_src->val;
 	//printf("%x\n",cpu.eip);
-	if(cpu.ZF==0||cpu.CF==0) cpu.eip += d;
+	if(cpu.ZF==0&&cpu.CF==0) cpu.eip += d;
 	//printf("%x\n %x\n",d,cpu.eip);
 	print_asm_template1();	
 }
