cc6e035b287da5e1b6972b728df2d65ff5468324
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-22 04:58:04 +0000

    >  run 3018001662 simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  04:58:04 up 7 days,  4:41,  1 user,  load average: 0.27, 0.20, 0.18 40adae366f84db8b73a01b1cf774b7bc73f9be

--- testcase/src/string.c
+++ testcase/src/string.c
@@ -21,7 +21,7 @@ int main() {
 	//nemu_assert(strcmp(s[0] + 3, s[1] + 3) == -1);
 
 	nemu_assert(strcmp( strcat(strcpy(str, str1), s[3]), s[4]) == 0);
-
+	memset(str,0,sizeof(str));
 	nemu_assert(memcmp(memset(str, '#', 5), s[5], 5) == 0);
 
 	return 0;
