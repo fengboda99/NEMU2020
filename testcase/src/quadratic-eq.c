ce111e3fdcc40d9d80c251416f2d3c8a6926029a
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-24 16:26:11 +0000

    >  run 3018001662 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:26:11 up 15 min,  1 user,  load average: 0.22, 0.28, 0.20 18f97f9bd34d281bcba0ba7b3b06e7c71ee729ca

--- testcase/src/quadratic-eq.c
+++ testcase/src/quadratic-eq.c
@@ -2,7 +2,11 @@
 #include "FLOAT.h"
 
 /* solve 0.5x^2 + 0.5x - 0.5 = 0 */
-
+FLOAT F_mul_F(FLOAT a, FLOAT b) {
+	//nemu_assert(0);
+	long long ans = 1ll*a*b;
+	return ans>>16;
+}
 int main() {
 	//FLOAT a = f2F(0.5);
 	//FLOAT b = f2F(3);
