4bec4c33df2e04b9fbdd144f4dfa52351c20216f
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-25 14:43:05 +0000

    >  compile NEMU 3018001662 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  14:43:05 up  8:25,  1 user,  load average: 0.19, 0.64, 0.54 b5354495faca144c5c88ce00f65c5c918b9081f0

--- nemu/src/monitor/debug/elf.c
+++ nemu/src/monitor/debug/elf.c
@@ -12,7 +12,7 @@ uint32_t getvalue(char* s,bool* success) {
  	*success = 1;
 	int i;
 	for(i=0;i<nr_symtab_entry;i++) {
-		if(symtab[i].st_info==STT_OBJECT) {
+		if((symtab[i].st_info&0xf)==STT_OBJECT) {
 			char tmp[30];
 			strcpy(tmp,strtab+symtab[i].st_name);
 			if(strcmp(tmp,s)==0) {
