a6e41aa8b461dc5140859b4cf91004510a52a647
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-18 07:02:47 +0000

    >  compile NEMU 3018001662 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  07:02:47 up 6 days,  1:42,  1 user,  load average: 0.53, 0.59, 0.64 d118451449bff8cb67469a7c21b99f406bb393d5

--- nemu/src/cpu/exec/string/stos-template.h
+++ nemu/src/cpu/exec/string/stos-template.h
@@ -3,31 +3,11 @@
 #define instr stos
 
 make_helper(concat(stos_1_,SUFFIX)) {
-	if(ops_decoded.is_operand_size_16)
-	{
-		swaddr_write(reg_w(R_DI),2,reg_w(R_AX),R_ES);
-		if(cpu.DF==0)
-		{
-			reg_w(R_DI)+=DATA_BYTE;
-		}	
-		else 
-		{
-			reg_w(R_DI)-=DATA_BYTE;	
-		}
-	}
-	else 
-	{
-		swaddr_write(reg_l(R_EDI),4,reg_l(R_EAX),R_ES);
-		if(cpu.DF==0)
-		{
-			reg_l(R_EDI)+=DATA_BYTE;
-		}	
-		else 
-		{
-			reg_l(R_EDI)-=DATA_BYTE;	
-		}
-	}	
+	MEM_W(reg_l(R_EDI),REG(R_EAX),R_ES);
+	if (cpu.DF == 0) reg_l(R_EDI) += DATA_BYTE;
+	else reg_l(R_EDI) -= DATA_BYTE;
 	print_asm_template2();	
+	
 	return 1;
 }
 #include "cpu/exec/template-end.h"
