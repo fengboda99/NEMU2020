150a4c739f30daa6b0555ddbc7363c4373b44f06
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-18 09:42:06 +0000

    >  compile kernel 3018001662 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  09:42:06 up 6 days,  4:21,  1 user,  load average: 0.65, 0.48, 0.31 16b7a25dedc65205841689d5dab4c57062fe6d37

--- kernel/src/memory/vmem.c
+++ kernel/src/memory/vmem.c
@@ -7,6 +7,7 @@
 
 /* Use the function to get the start address of user page directory. */
 PDE* get_updir();
+static PTE vtable[1024] align_to_page;
 
 void create_video_mapping() {
 	/* TODO: create an identical mapping from virtual memory area 
@@ -14,7 +15,16 @@ void create_video_mapping() {
 	 * [0xa0000, 0xa0000 + SCR_SIZE) for user program. You may define
 	 * some page tables to create this mapping.
 	 */
-	panic("please implement me");
+	//panic("please implement me");
+	PDE* tmp = (PDE*)va_to_pa(get_updir());
+	tmp[0].page_frame = (uint32_t)va_to_pa(vtable)>>12;
+	tmp[0].present = 1;
+	int i;
+	for(i=VMEM_ADDR/PAGE_SIZE;i<(VMEM_ADDR+SCR_SIZE)/PAGE_SIZE;i++) {
+		vtable[i].page_frame = i;
+		vtable[i].present = 1;
+	}
+
 }
 
 void video_mapping_write_test() {
