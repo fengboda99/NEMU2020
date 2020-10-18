8ce4256ac823924a0882d05c9a383c5c9878b68f
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-18 08:07:34 +0000

    >  compile kernel 3018001662 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  08:07:34 up 6 days,  2:46,  1 user,  load average: 0.54, 0.59, 0.68 40196339bf6e3bedb57102c15ac099ab95f3141

--- kernel/src/memory/kvm.c
+++ kernel/src/memory/kvm.c
@@ -44,8 +44,8 @@ void init_page(void) {
 	
 		//===== referenced code for the inline assembly above =====
 
-		uint32_t pframe_addr = PHY_MEM - PAGE_SIZE;
-		//ptable --;
+		uint64_t pframe_addr = PHY_MEM - PAGE_SIZE;
+		ptable --;
 
 		// fill PTEs reversely
 		for (; pframe_addr >= 0; pframe_addr -= PAGE_SIZE) {
