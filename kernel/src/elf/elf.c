e2042e663d95e319a73cf8cfa8be01e40705fd7f
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-18 07:34:01 +0000

    >  compile NEMU 3018001662 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  07:34:01 up 6 days,  2:13,  1 user,  load average: 0.90, 0.42, 0.43 ee5c03ae3888d12bbcdb1dc5ad8bcd32eef91e55

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -42,7 +42,7 @@ uint32_t loader() {
 	for(i=0;i<elf->e_phnum;i++,ph++) {
 		/* Scan the program header table, load each segment into memory */
 		if(ph->p_type == PT_LOAD) {
-
+			ph->p_vaddr = mm_malloc(ph->p_vaddr, ph->p_memsz);
 			/* TODO: read the content of the segment from the ELF file 
 			 * to the memory region [VirtAddr, VirtAddr + FileSiz)
 			 */
