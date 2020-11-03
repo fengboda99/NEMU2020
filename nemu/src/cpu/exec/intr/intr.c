cfba48aef666d7e7704130639325fa332bf55e64
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-03 04:49:15 +0000

    >  compile NEMU 3018001662 simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  04:49:15 up 19:37,  1 user,  load average: 0.79, 0.69, 0.41 5ffda54c954b4d2b37db11a1325996efc54f57f

--- nemu/src/cpu/exec/intr/intr.c
+++ nemu/src/cpu/exec/intr/intr.c
@@ -1,11 +1,13 @@
 #include <setjmp.h>
+#include "nemu.h"
 extern jmp_buf jbuf;
 
-void raise_intr(uint8_t) {
+void raise_intr(uint8_t NO) {
 
 	/*TODO: Trigger an interrupt
 	 */
-	
+	printf("%d\n",NO);
 	/* Jump back to cpu_exec() */
 	longjmp(jbuf,1);
 }
+
