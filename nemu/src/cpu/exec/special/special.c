e719ac08e9ae482eb1b055b61ef63de773784c78
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-09 06:49:53 +0000

    >  compile NEMU 3018001662 simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  06:49:53 up  1:26,  1 user,  load average: 0.36, 0.60, 0.48 d3df65e0196e97e6f17fec698294193424f77a

--- nemu/src/cpu/exec/special/special.c
+++ nemu/src/cpu/exec/special/special.c
@@ -28,9 +28,12 @@ make_helper(nemu_trap) {
 	print_asm("nemu trap (eax = %d)", cpu.eax);
 
 	switch(cpu.eax) {
-		case 2:
+		case 2: {
+			int i;
+ 			for (i = 0;i < cpu.edx;i++)
+ 			putchar(swaddr_read(cpu.ecx + i,1,R_DS));
 		   	break;
-
+			}
 		default:
 			printf("\33[1;31mnemu: HIT %s TRAP\33[0m at eip = 0x%08x\n\n",
 					(cpu.eax == 0 ? "GOOD" : "BAD"), cpu.eip);
