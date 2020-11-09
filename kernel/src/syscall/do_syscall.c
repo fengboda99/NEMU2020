8a39e4352c17d32d9ca8bf5aa311f758b7843024
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-09 07:42:09 +0000

    >  compile kernel 3018001662 simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  07:42:09 up  2:18,  1 user,  load average: 0.46, 0.52, 0.47 b9c717de36ea4732933ccc18ead2d9c43fc7b916

--- kernel/src/syscall/do_syscall.c
+++ kernel/src/syscall/do_syscall.c
@@ -46,9 +46,8 @@ void do_syscall(TrapFrame *tf) {
 			break;
 
 		case SYS_brk: sys_brk(tf); break;
-		case SYS_write: sys_write(tf); break;
 		case SYS_ioctl: sys_ioctl(tf); break;
-
+		case SYS_write: sys_write(tf); break;
 		/* TODO: Add more system calls. */
 
 		default: panic("Unhandled system call: id = %d, eip = 0x%08x", tf->eax, tf->eip);
