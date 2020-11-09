fd2c21a311cf392e5e96de9988737175bab527f1
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-09 11:31:29 +0000

    >  compile NEMU 3018001662 simpleedu Linux simpleedu 4.15.0-122-generic #124-Ubuntu SMP Thu Oct 15 13:03:05 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  11:31:29 up 14 min,  1 user,  load average: 0.19, 0.23, 0.19 c37b6724401933fa88733b2adbd900bfbe93ccc1

--- nemu/src/cpu/exec/intr/iret.c
+++ nemu/src/cpu/exec/intr/iret.c
@@ -1,21 +1,22 @@
 #include "cpu/exec/helper.h"
 #include "nemu.h"
-int pop1() {
+int pop() {
 	uint32_t tmp = swaddr_read(reg_l(R_ESP),4,R_SS);
 	reg_l(R_ESP)+=4;
+	printf("%d\n",tmp);
 	return tmp;
 }
 make_helper(iret) {
 	if(cpu.cr0.protect_enable==0) {
-		cpu.eip=pop1();
-		cpu.cs.selector = pop1();
-		cpu.eflags = pop1();	
+		cpu.eip=pop();
+		cpu.cs.selector = pop();
+		cpu.eflags = pop();	
 	}
 	else {
 		//printf("1\n");
-		cpu.eip=pop1();
-		cpu.cs.selector = pop1();
-		cpu.eflags = pop1();
+		cpu.eip=pop();
+		cpu.cs.selector = pop();
+		cpu.eflags = pop();
 		//printf("13\n");
 		seg_do(R_CS);
 	}	
