d6c9b10db48aaa11bf6f6b81b389208cc75094d8
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-09 11:39:32 +0000

    >  compile NEMU 3018001662 simpleedu Linux simpleedu 4.15.0-122-generic #124-Ubuntu SMP Thu Oct 15 13:03:05 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  11:39:32 up 22 min,  1 user,  load average: 0.16, 0.25, 0.20 312762aed06337abb7b2448d98098469ba4a175f

--- nemu/src/cpu/exec/data-mov/popa-template.h
+++ nemu/src/cpu/exec/data-mov/popa-template.h
@@ -6,9 +6,9 @@ make_helper(concat(popa_1_,SUFFIX)) {
 	int i;
 	for(i=R_DI;i>=R_AX;i--) {
 		if(i!=4) {
-			REG(i) = MEM_R(REG(R_SP),R_SS);
-			REG(R_SP)+=DATA_BYTE;		
+			REG(i) = MEM_R(REG(R_SP),R_SS);	
 		}	
+		REG(R_SP)+=DATA_BYTE;	
 	}
 	return 1;
 }
