a715b2d8d7d8780fa3941f98826c89cf414288c7
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-10 09:33:21 +0000

    >  compile kernel 3018001662 simpleedu Linux simpleedu 4.15.0-122-generic #124-Ubuntu SMP Thu Oct 15 13:03:05 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  09:33:21 up 22:16,  1 user,  load average: 1.02, 0.94, 0.68 e607f046835fd7a4a55e8c4de9bf3163747c769

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -80,7 +80,7 @@ void init_cond() {
 
 	/* Load the program. */
 	uint32_t eip = loader();
-	
+	set_bp();
 #if defined(IA32_PAGE) && defined(HAS_DEVICE)
 	/* Read data in the video memory to check whether 
 	 * the test data is written sucessfully.
