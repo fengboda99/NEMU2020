bb7d2bd54d88e702d4590bcdbdb7f506414f7315
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-10 11:47:15 +0000

    >  compile kernel 3018001662 simpleedu Linux simpleedu 4.15.0-122-generic #124-Ubuntu SMP Thu Oct 15 13:03:05 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  11:47:15 up 1 day, 30 min,  1 user,  load average: 0.81, 1.00, 0.88 451a34d5d18e162c959dd80d9deb82a24ff88c32

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -72,7 +72,7 @@ void init_cond() {
 	 * the serial port is available in NEMU.
 	 */
 	Log("Hello, NEMU world!");
-
+	set_bp();
 #if defined(IA32_PAGE) && defined(HAS_DEVICE)
 	/* Write some test data to the video memory. */
 	video_mapping_write_test();
