b8c9c74e5d72e23fa652f502ed17bd9fa7ff5955
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-11 11:34:19 +0000

    >  compile kernel 3018001662 simpleedu Linux simpleedu 4.15.0-122-generic #124-Ubuntu SMP Thu Oct 15 13:03:05 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  11:34:19 up 2 days, 17 min,  1 user,  load average: 0.39, 0.60, 0.72 cb7890d3371d00678023d23d99e432838314fca2

--- kernel/src/syscall/do_syscall.c
+++ kernel/src/syscall/do_syscall.c
@@ -18,8 +18,8 @@ static void sys_write(TrapFrame *tf) {
 	uint32_t ans = 0;
 	if(fd==1||fd==2) {
 		//set_bp();
-		//asm volatile (".byte 0xd6" :: "a"(2), "c"(buf), "d"(len));	
-		while(len--) serial_printc(*(buf++));	
+		asm volatile (".byte 0xd6" :: "a"(2), "c"(buf), "d"(len));	
+		//while(len--) serial_printc(*(buf++));	
 		ans = tf->edx;
 		//set_bp();
 	}
