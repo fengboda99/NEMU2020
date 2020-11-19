3b1601901ac005e17d1bea1264160ffb969b7cf8
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-19 01:48:54 +0000

    >  run 3018001662 simpleedu Linux simpleedu 4.15.0-123-generic #126-Ubuntu SMP Wed Oct 21 09:40:11 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  01:48:54 up 14:45,  1 user,  load average: 0.96, 0.76, 0.47 acf7267ae01a74f1addd984bf12a9c2663fe649c

--- game/src/nemu-pal/main.c
+++ game/src/nemu-pal/main.c
@@ -515,7 +515,7 @@ main_loop() {
 
 #if defined(__APPLE__) && !defined(__IOS__)
    char *p = strstr(argv[0], "/Pal.app/");
-
+   set_bp();
    if (p != NULL)
    {
       char buf[4096];
@@ -524,9 +524,9 @@ main_loop() {
       chdir(buf);
    }
 #endif
-
-#ifdef __WINPHONE__
    set_bp();
+#ifdef __WINPHONE__
+
    SDL_SetHint(SDL_HINT_ORIENTATIONS, "LandscapeRight");
    SDL_SetHint(SDL_HINT_WINRT_HANDLE_BACK_BUTTON, "1");
 #endif
